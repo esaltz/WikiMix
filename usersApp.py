f2328fc5651bacae7a6e1ebb8cb19bb063b1943b
esaltz <108403662+esaltz@users.noreply.github.com>
2024-05-16 06:06:56 -0700

    Update usersApp.py

--- usersApp.py
+++ usersApp.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-from st_login_form import login_form
 from st_supabase_connection import SupabaseConnection
 
 # Initialize connection.
@@ -14,13 +13,3 @@ for row in rows.data:
     st.write(f"{row['name']} has a :{row['pet']}:")
 
 st.title("Title")
-
-client = login_form()
-
-if st.session_state["authenticated"]:
-    if st.session_state["username"]:
-        st.success(f"Welcome {st.session_state['username']}")
-    else:
-        st.success("Welcome guest")
-else:
-    st.error("Not authenticated")
